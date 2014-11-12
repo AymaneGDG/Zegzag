4bc6bf534da93b3be6d960f2e30eb345d858c87b
Aymane GDG <hadri.1.aymane@gmail.com>
2014-11-12 22:06:05 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+We want to divide a Pizza and finding the maximum number of pieces that we can get using a Knife that can only cut the whole Pizza K times
+(All cuts must be from one side to the other side, and the pieces can have different sizes)
---
+++ pizza.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main() {
+	//freopen("pizza.in","rt",stdin);
+	//freopen("pizza.out","wt",stdout);
+	
+	
+	
+	long  int k  ;
+	
+	while(scanf("%ld",&k) == 1) 
+	{
+	
+	
+	if(k>=0)
+	
+	
+	printf("%ld\n",(k*k+k+2)/2) ;
+	
+    else
+		break ;
+	
+	
+    }
+    
+	return 0 ;
+	}
+
