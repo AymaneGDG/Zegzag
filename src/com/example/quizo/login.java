3e7695b9309caee45e2a5d7194039e4bd127b335
Aymane GDG <hadri.1.aymane@gmail.com>
2014-11-20 21:39:36 +0000

    First Commit

---
+++ src/com/example/quizo/login.java
@@ -0,0 +1,19 @@
+package com.example.quizo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.content.Intent;
+import android.widget.*;
+
+public class login extends Activity {
+	 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+
+}
