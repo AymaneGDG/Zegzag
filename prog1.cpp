d86b81cc095300c370b578727246d8b580e7e92e
Aymane GDG <hadri.1.aymane@gmail.com>
2014-11-21 21:12:32 +0000

    Commit First

--- prog1.cpp
+++ prog1.cpp
@@ -1,36 +1,50 @@
+#include <fstream>
 #include <iostream>
- 
 using namespace std;
  
-class Box
+int main ()
 {
-   public:
-      double length;   // Length of a box
-      double breadth;  // Breadth of a box
-      double height;   // Height of a box
-};
+    
+   char data[100];
  
-int main( )
-{
-   Box Box1;        // Declare Box1 of type Box
-   Box Box2;        // Declare Box2 of type Box
-   double volume = 0.0;     // Store the volume of a box here
+   // open a file in write mode.
+   ofstream outfile;
+   outfile.open("afile.dat");
  
-   // box 1 specification
-   Box1.height = 5.0; 
-   Box1.length = 6.0; 
-   Box1.breadth = 7.0;
+   cout << "Writing to the file" << endl;
+   cout << "Enter your name: "; 
+   cin.getline(data, 100);
  
-   // box 2 specification
-   Box2.height = 10.0;
-   Box2.length = 12.0;
-   Box2.breadth = 13.0;
-   // volume of box 1
-   volume = Box1.height * Box1.length * Box1.breadth;
-   cout << "Volume of Box1 : " << volume <<endl;
+   // write inputted data into the file.
+   outfile << data << endl;
  
-   // volume of box 2
-   volume = Box2.height * Box2.length * Box2.breadth;
-   cout << "Volume of Box2 : " << volume <<endl;
+   cout << "Enter your age: "; 
+   cin >> data;
+   cin.ignore();
+   
+   // again write inputted data into the file.
+   outfile << data << endl;
+ 
+   // close the opened file.
+   outfile.close();
+ 
+   // open a file in read mode.
+   ifstream infile; 
+   infile.open("afile.dat"); 
+ 
+   cout << "Reading from the file" << endl; 
+   infile >> data; 
+ 
+   // write the data at the screen.
+   cout << data << endl;
+   
+   // again read the data from the file and display it.
+   infile >> data; 
+   cout << data << endl; 
+ 
+   // close the opened file.
+   infile.close();
+ 
    return 0;
 }
+
